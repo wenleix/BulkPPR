c47cf740f70a9948792cbc153cb98dbdb7407c9e
Wenlei Xie <wenlei.xie@gmail.com>
2014-03-12 16:59:19 -0700

    some preprocess.

---
+++ misc/convertgraph.cpp
@@ -0,0 +1,72 @@
+//  Preprocess the dataset from Jure's website. 
+//  Relabel the dataset and convert it into Wenlei Graph format
+
+//  Wenlei Graph Format:
+//  [NumVertex] [NumEdge]
+//  [OutDeg] [DstID1] [DstID2]...
+//  [OutDeg] [DstID1] [DstID2]...
+
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+const int MaxOriginalID = 80000000;
+const int MaxNumVertex = 2000000;   //  2M vertices atm.
+int mapping[MaxOriginalID];
+vector<int> outEdge[MaxNumVertex];
+
+int newIDCnt = 0;
+
+int relabel(int u) {
+    int *newID = &mapping[u];
+    if (*newID == -1) 
+        *newID = newIDCnt++;
+    
+    return *newID;
+}
+
+int main(int argv, char** argc) {
+    memset(mapping, 0xFF, sizeof(mapping));
+
+    if (argv != 3) {
+        fprintf(stderr, "Usage: %s [JureData] [OutData]\n", argc[0]);
+        return 1;
+    }
+    
+    FILE *inFile = fopen(argc[1], "r");
+    FILE *outFile = fopen(argc[2], "w");
+
+    char buf[1000];
+    int u, v;
+    int edgeCnt = 0;
+
+    while (fgets(buf, 1000, inFile) != NULL) {
+        if (buf[0] == '#') 
+            continue;
+
+        sscanf(buf, "%d%d", &u, &v);
+        int uu = relabel(u);
+        int vv = relabel(v);
+        outEdge[uu].push_back(vv);
+        edgeCnt++;
+    }
+
+    //  Print the output file
+    fprintf(outFile, "%d %d\n", newIDCnt, edgeCnt);
+    for (int i = 0; i < newIDCnt; i++) {
+        fprintf(outFile, "%lu", outEdge[i].size());
+        for (int j = 0; j < outEdge[i].size(); j++)
+            fprintf(outFile, " %d", outEdge[i][j]);
+        fprintf(outFile, "\n");
+    }
+
+    fclose(inFile);
+    fclose(outFile);
+
+    fprintf(stdout, "INFO: Vertex: %d, Edge: %d\n", newIDCnt, edgeCnt);
+
+    return 0;
+}
+
