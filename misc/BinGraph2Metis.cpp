7b6a519b64e72a6bc8d2519db25fa06368183311
Wenlei Xie <wenlei.xie@gmail.com>
2014-03-31 15:14:34 -0400

    Sort/uniq complete

--- misc/BinGraph2Metis.cpp
+++ misc/BinGraph2Metis.cpp
@@ -1,10 +1,11 @@
 //  Convert the binary version of graph to the METIS format :)
-//  Since for large graphs I am using binary format :)
+//  Since for large graphs I am using binary format, and use array/sort/unique instead of hash_set
 
 #include <cstdio>
 #include <cstdlib>
 #include <cassert>
-#include <unordered_set>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -24,24 +25,35 @@ int main(int argc, char** argv) {
     numRead = fread(&numEdge, sizeof(int), 1, binFile);
     assert(numRead == 1);
 
-	unordered_set<int>* neighborVertex = new unordered_set<int>[numVtx];
+    vector<int>* neighborVertex = new vector<int>[numVtx];
     for (int i = 0; i < numVtx; i++) {
         int outDeg = 0;
         numRead = fread(&outDeg, sizeof(int), 1, binFile);
         assert(numRead == 1);
 
+        neighborVertex[i].reserve(outDeg);
         int *outId = new int[outDeg];
         numRead = fread(outId, sizeof(int), outDeg, binFile);
+        assert(numRead == outDeg);
+
         for (int j = 0; j < outDeg; j++) {
             int u1 = i;
             int u2 = outId[j];
-            neighborVertex[u1].insert(u2);
-            neighborVertex[u2].insert(u1);
+            neighborVertex[u1].push_back(u2);
+            neighborVertex[u2].push_back(u1);
         }
-
         delete [] outId;
+
     }
 	fprintf(stderr, "Read complete!\n");
+
+    //  Sort and uniq
+    for (int i = 0; i < numVtx; i++) {
+        sort(neighborVertex[i].begin(), neighborVertex[i].end());
+        neighborVertex[i].resize(unique(neighborVertex[i].begin(), neighborVertex[i].end()) - neighborVertex[i].begin());
+    }
+
+	fprintf(stderr, "Sort/Uniq complete!\n");
 
 	//	Count # edges
 	int countEdges = 0;
@@ -54,7 +66,7 @@ int main(int argc, char** argv) {
 
 	fprintf(metisFile, "%d %d\n", numVtx, countEdges / 2);
 	for (int i = 0; i < numVtx; i++) {
-		for (unordered_set<int>::iterator it = neighborVertex[i].begin();
+		for (auto it = neighborVertex[i].begin();
 				it != neighborVertex[i].end(); it++) {
 			//	Metis doesn't require the vertex list to be sorted
 			fprintf(metisFile, "%d ", (*it) + 1);
